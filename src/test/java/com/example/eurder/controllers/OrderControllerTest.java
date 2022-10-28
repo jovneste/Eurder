c5f3c83f472e97b2192a16e48ba24e3b95a4aefe
Jordy Vanneste <jordyvanneste@hotmail.com>
2022-10-28 13:52:55 +0200

    new test wrong passwordexception

--- src/test/java/com/example/eurder/controllers/OrderControllerTest.java
+++ src/test/java/com/example/eurder/controllers/OrderControllerTest.java
@@ -3,16 +3,19 @@ package com.example.eurder.controllers;
 
 import com.example.eurder.domain.dtos.NewOrderDto;
 import com.example.eurder.domain.dtos.ReturnOrderDto;
+import com.example.eurder.exceptions.securityexceptions.WrongPasswordException;
 import com.example.eurder.repositories.OrderRepository;
 import com.example.eurder.repositories.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+import java.util.Base64;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -68,6 +71,30 @@ class OrderControllerTest {
                 .assertThat()
                 .statusCode(HttpStatus.UNAUTHORIZED.value())
                 .extract();
+
+    }
+    @Test
+    void WrongPasswordMakesAnOrder_ThrowsWrongPasswordException(){
+
+        String requestedBody = "{\"customer\":{\"firstName\":\"a\",\"lastName\":\"b\",\"emailAdress\":\"A@B.com\",\"address\":\"str\",\"phoneNumber\":\"555\",\"password\":\"pass\"},\"itemGroupList\":[{\"item\":{\"name\":\"phoenixdown\",\"description\":\"item\",\"price\":5,\"amountInStock\":4},\"amountToOrder\":10}]}";
+        RestAssured
+                .given()
+                .auth()
+                .preemptive()
+                .basic("customer@eurder.com","wrongpassword")
+                .contentType(ContentType.JSON)
+                .baseUri("http://localhost")
+                .port(port)
+                .body(requestedBody)
+                .when()
+                .accept(ContentType.JSON)
+                .post("/orders/")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .extract();
+
+
 
     }
 
